5a0c2f91f53e31b88b4254c398c21f605ef7a77b
Salsedini <d.s.022222@gmail.com>
2023-11-22 15:13:15 +0100

    feat: add bookForm page
    Co-authored-by: 02Alvaro <02Alvaro@users.noreply.github.com>

---
+++ src/app/book/new/page.tsx
@@ -0,0 +1,12 @@
+import BookForm from '@/components/BookForm/BookForm'
+
+export default async function Page() {
+  return (
+    <main>
+      <h1>
+        <strong>Crear Nuevo Libro</strong>
+      </h1>
+      <BookForm />
+    </main>
+  )
+}

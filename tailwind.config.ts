91b264fd8d43ebd13bd9ffbe5f5a79ddf2a5c1d5
Sergio Gómez Bachiller <sergio@uco.es>
2024-04-18 11:23:12 +0200

    chore: add progress component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/components/(avatar|breadcrumbs|button|card|divider|dropdown|input|kbd|link|listbox|modal|navbar|toggle|table|tabs|user|ripple|spinner|menu|popover|checkbox|spacer).js',
+    './node_modules/@nextui-org/theme/dist/components/(avatar|breadcrumbs|button|card|divider|dropdown|input|kbd|link|listbox|modal|navbar|progress|toggle|table|tabs|user|ripple|spinner|menu|popover|checkbox|spacer).js',
   ],
   darkMode: 'class',
   plugins: [nextui()],

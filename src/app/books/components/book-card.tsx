5e94e3e6aa10bc3f2e96ca7c54de54b1ffec1249
Sergio Gómez Bachiller <sergio@uco.es>
2024-04-28 23:42:49 +0200

    fix: scroll to top of book view

--- src/app/books/components/book-card.tsx
+++ src/app/books/components/book-card.tsx
@@ -40,6 +40,7 @@ export function BookCard(properties: BookCardProperties) {
         prefetch
         radius="none"
         role="gridcell"
+        scroll={false}
       >
         <div className="relative">
           <Image
